8e08a83be79a7dd2e3571e35e01077a3c595d02a
yiteng3 <yiteng3@illinois.edu>
2022-09-22 20:02:00 -0500

    update Main and add Message

--- src/main/java/service/Main.java
+++ src/main/java/service/Main.java
@@ -2,18 +2,92 @@ package service;
 
 import com.google.protobuf.Timestamp;
 import core.Process;
-import core.ProcessStatus;
 
+import java.io.IOException;
+import java.net.InetAddress;
 import java.time.Instant;
+import java.util.LinkedList;
+import java.util.Properties;
+import java.util.Scanner;
 
 /**
  * Main class, response to console command
  */
 public class Main {
-    public static void main(String[] args) {
+    // membership list
+    static LinkedList<Process> membershipList = new LinkedList<>();
+    // properties file path
+    static String propertiesPath = "/resources/setting.properties";
+    // ack list
+    static boolean[] isAck;
+    static int monitorRange;
+    static String introducer;
+    static int port;
+    private final String hostName;
+    private final Timestamp timestamp;
+
+    private Main() throws IOException {
+        Properties properties = new Properties();
+        properties.load(this.getClass().getResourceAsStream(propertiesPath));
+        monitorRange = Integer.parseInt(properties.getProperty("monitor_range"));
+        isAck = new boolean[monitorRange];
+        introducer = properties.getProperty("introducer");
+        port = Integer.parseInt(properties.getProperty("port"));
+        InetAddress address = InetAddress.getLocalHost();
+        this.hostName = address.getHostName();
         Instant time = Instant.now();
-        Process process = Process.newBuilder().setAddress("123").setPort(11).setStatus(ProcessStatus.ALIVE)
-                .setTimestamp(Timestamp.newBuilder().setSeconds(time.getEpochSecond()).setNanos(time.getNano())).build();
-        System.out.println(process);
+        this.timestamp = Timestamp.newBuilder().setSeconds(time.getEpochSecond()).build();
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        Scanner scanner = new Scanner(System.in);
+        String command;
+        while (!(command = scanner.nextLine()).equals("EOF")) {
+            switch (command) {
+                case "join":
+                    main.join();
+                    break;
+                case "leave":
+                    main.leave();
+                    break;
+                case "list_mem":
+                    main.display();
+                    break;
+                case "list_self":
+                    main.print();
+                    break;
+                default:
+                    System.out.println("wrong command, please re-input");
+            }
+        }
+        main.join();
+    }
+
+    private void display() {
+        System.out.println("-----------------------------------");
+        System.out.println("-         membership list         -");
+        System.out.println("-----------------------------------");
+        for (Process process:membershipList) {
+            System.out.println(process);
+        }
+        System.out.println("-----------------------------------");
+    }
+
+    private void print() {
+        System.out.println("Self ID: " + hostName + "@" + timestamp.getSeconds());
+    }
+
+    private void leave() {
+        //TODO call sender to inform others
+        System.out.println("leaved the group!");
+    }
+
+    private void join() {
+        //TODO call introducer to get list
+
+        //TODO call sender to inform others
+        System.out.println("joined into the group!");
     }
 }
